438b8c6139394d097b95bc50dd78c868ed262877
Andrew <aeason96@vt.edu>
2018-07-23 10:53:05 -0400

    Basic Testing Setup

---
+++ test/temp-convertor.spec.js
@@ -0,0 +1,7 @@
+describe('TempConverter', function () {
+    describe('convertCtoF', function () {
+        it('should return 212f given 100c', function () {
+            expect(100).toEqual(212);
+        });
+    });
+});
---
+++ test/temp-convertor.spec.ts
@@ -0,0 +1,7 @@
+describe('TempConverter', () => {
+    describe('convertCtoF', () => {
+        it('should return 212f given 100c', () => {
+            expect(100).toEqual(212)
+        })
+    })
+});
